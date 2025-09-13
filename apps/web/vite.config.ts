9149333388c47f6af7939ec29e8f90e241b39161
Allen Alper <allenalper@me.com>
2025-09-12 21:12:42 -0400

    chore(web): set vite base to /susscanner-monorepo/

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/susscanner/', // IMPORTANT for GitHub Pages
+  base: '/susscanner-monorepo/',  // must match your GitHub repo name
 })
